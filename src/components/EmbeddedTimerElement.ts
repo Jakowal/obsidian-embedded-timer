0a1adfd252d163f040f7d3b99abe216dd8c8eb83
Jakob <jakob.walmann@outlook.com>
2024-06-17 21:30:08 +0200

    Initial functioning countdown

--- src/components/EmbeddedTimerElement.ts
+++ src/components/EmbeddedTimerElement.ts
@@ -1,5 +1,51 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import Timer from 'main';
+import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, setIcon } from 'obsidian';
+import { TimerModel } from 'src/models/timer.model';
+import { getTimerString, timerIsFinished, updateSecondsLeftForTimer } from 'src/utils/timer.utils';
+import { timeFormatIsValid, timeInputMatchesFormat } from 'src/utils/validators';
 
-export class EmbeddedTimerElement {
+export const embeddedTimerElement = (el: HTMLElement, component: Timer, timer: TimerModel) => {
+
+    if (!timeFormatIsValid(timer.formatString)) {
+        new Notice("Invalid time format...", 2000);
+        return;
+    }
+
+    if (!timeInputMatchesFormat(timer.timeString, timer.formatString)) {
+        new Notice("Time input doesn't match the format...", 2000);
+        return;
+    }
+
+    const timerContainer = el.createDiv("embedded-timer-wrapper").createDiv("timer-container");
+
+    const timeElement = timerContainer.createSpan("time");
+    timeElement.setText(getTimerString(timer.secondsLeft, timer.formatString));
     
+    let runTimer = -99;
+
+    const timeControls = timerContainer.createSpan("time-controls");
+
+    const playPauseButton = timeControls.createEl("button", "timer-btn play-pause-btn");
+    setIcon(playPauseButton, "play");
+
+    const resetButton = timeControls.createEl("button", "timer-btn reset-btn");
+    setIcon(resetButton, "rotate-ccw");
+
+    const toggleTimer = () => {
+        if (runTimer !== -99) {
+            setIcon(playPauseButton, "play");
+            window.clearInterval(runTimer);
+            runTimer = -99;
+            timer.lastUpdated = undefined;
+        }
+        else {
+            setIcon(playPauseButton, "pause");
+            runTimer = window.setInterval(() => updateSecondsLeftForTimer(timer, timeElement), 500);
+            component.registerInterval(runTimer);
+            timer.lastUpdated = new Date();
+        }
+    }
+
+    playPauseButton.onClickEvent(toggleTimer);
+
 }
--- src/utils/timer.utils.ts
+++ src/utils/timer.utils.ts
@@ -1,9 +1,86 @@
+import Timer from "main";
 import { TimerModel } from "src/models/timer.model";
+import { TimeFormatRegex } from "./validators";
+import { Notice } from "obsidian";
 
-export const timerIsFinished = (timer: TimerModel, onComplete?: () => void): boolean => {
-    const currentTime = new Date();
+export const timerIsFinished = (timer: TimerModel): boolean => timer.secondsLeft === 0;
 
-    
+export const updateSecondsLeftForTimer = (timer: TimerModel, timerTextSpan: HTMLSpanElement) => {
+    if (!timerIsFinished(timer)) {
+        if (timer.lastUpdated) {
+            const now = Math.ceil(new Date().getTime() / 1000);
+            const lastUpdate = Math.ceil(timer.lastUpdated.getTime() / 1000);
+            if (now - lastUpdate > 0) {
+                timer.secondsLeft -= (now - lastUpdate);
+                timer.lastUpdated = new Date();
+                timerTextSpan.setText(getTimerString(timer.secondsLeft, timer.formatString));
+            }
+        }
+    }
+    else {
+        new Notice("Timer finished!!");
+    }
+}
 
-    return false;
+export const getTimerString = (secondsLeft: number, formatString: string): string => {
+    let seconds = secondsLeft;
+
+    let secondsString = "";
+    let hoursString = "";
+    let minutesString = "";
+
+    if (TimeFormatRegex.hours.test(formatString)) {
+        hoursString = `${Math.floor(seconds/3600)}`
+        if (hoursString.length === 1) {
+            hoursString = `0${hoursString}`
+        }
+        seconds -= +hoursString * 3600;
+    }
+    if (TimeFormatRegex.minutes.test(formatString)) {
+        minutesString = `${Math.floor(seconds/60)}`;
+        if (minutesString.length === 1) {
+            minutesString = `0${minutesString}`
+        }
+        seconds -= +minutesString * 60;
+    }
+
+    if (TimeFormatRegex.seconds.test(formatString)) {
+        secondsString = `${seconds}`;
+        if (secondsString.length === 1) {
+            secondsString = `0${secondsString}`
+        }
+    }
+    return formatString
+        .replace(TimeFormatRegex.hours, hoursString)
+        .replace(TimeFormatRegex.minutes, minutesString)
+        .replace(TimeFormatRegex.seconds, secondsString);
+}
+
+const getSecondsForTimer = (timeString: string, formatString: string): number => {
+    const hoursIndex = TimeFormatRegex.hours.exec(formatString)?.index;
+    const minutesIndex = TimeFormatRegex.minutes.exec(formatString)?.index;
+    const secondsIndex = TimeFormatRegex.seconds.exec(formatString)?.index;
+
+    let totalSeconds = 0;
+
+    if (hoursIndex !== undefined) {
+        totalSeconds += +timeString.slice(hoursIndex, hoursIndex + 2) * 3600;
+    }
+    if (minutesIndex !== undefined) {
+        totalSeconds += +timeString.slice(minutesIndex, minutesIndex + 2) * 60;
+    }
+    if (secondsIndex !== undefined) {
+        totalSeconds += +timeString.slice(secondsIndex, secondsIndex + 2);
+    }
+    return totalSeconds;
+}
+
+export const createTimerObject = (config: string[], component: Timer): TimerModel => {
+    const format = config[1] || component.settings.timeFormat;
+
+    return {
+        timeString: config[0],
+        formatString: format,
+        secondsLeft: getSecondsForTimer(config[0], format),
+    }
 }
